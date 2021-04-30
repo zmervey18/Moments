4ad9ebc35ba9130137990389772904e6677b3b30
Lionel Smith <smithlionel1121@gmail.com>
2021-04-30 17:23:03 +0100

    Add moments and profile models
    Co-authored-by: Kashan Sheikh <k-sheikh@users.noreply.github.com>

--- back_end/moments/journal/models.py
+++ back_end/moments/journal/models.py
@@ -1,9 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_photo = models.ImageField(upload_to='profile_image', blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
 class Prompt(models.Model):
-    prompt = models.CharField(max_length=100)
+    prompt = models.CharField(max_length=200)
 
     def __str__(self):
         return self.prompt
+
+class Moment(models.Model):
+    entry_date = models.DateField(auto_now_add=True)
+    entry_title = models.CharField(max_length=100)
+    moment_entry = models.TextField()
+    moment_photo = models.ImageField(upload_to='moments')
+    owner = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name="moments")
+    prompt = models.ForeignKey("Prompt", on_delete=models.DO_NOTHING, related_name="moments")
