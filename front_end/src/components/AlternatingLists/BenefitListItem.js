00dc37cd39c8aa030b72076406e39dd018b45c52
Lionel Smith <smithlionel1121@gmail.com>
2021-04-30 19:21:44 +0100

    Implement alternating list structure

--- front_end/src/components/AlternatingLists/BenefitListItem.js
+++ front_end/src/components/AlternatingLists/BenefitListItem.js
@@ -1,19 +1,24 @@
-import './BenefitListItem.css'
+import "./BenefitListItem.css";
 
-const BenefitListItem = ({image, subtitle, alt, textClass}) => {
-    return (
-        <div className="BenefitListCard">
-            <img 
-            src={image} 
-            alt={alt}
-            className="BenefitListImage"
-            >
-            </img>
-            <div className={textClass}>
-            <h3>{subtitle}</h3>
-            </div>
-        </div>
-    )
-}
+const BenefitListItem = ({
+  image,
+  subtitle,
+  alt,
+  textClass,
+  reverse = false,
+}) => {
+  return (
+    <div className="BenefitListCard">
+      <img
+        src={image}
+        alt={alt}
+        className={`BenefitListImage ${reverse ? "second" : "first"}`}
+      ></img>
+      <div className={`${textClass} ${reverse ? "first" : "second"}`}>
+        <h3>{subtitle}</h3>
+      </div>
+    </div>
+  );
+};
 
-export default BenefitListItem
+export default BenefitListItem;
