e02ea5e8687d643b84fa7f9b09da09d7c3cd57ab
Lionel Smith <smithlionel1121@gmail.com>
2021-05-03 05:20:55 +0100

    Restrict permissions on journal viewsets

--- back_end/moments/journal/views.py
+++ back_end/moments/journal/views.py
@@ -9,21 +9,44 @@ User = get_user_model()
 
 # Create your views here.
 
+class IsOwner(permissions.BasePermission):
+    """
+    The request is authenticated as an owner.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    The request is authenticated as an admin, or is a read-only request.
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and request.user.is_staff and
+            request.user.is_authenticated
+        )
+
+
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
 
 class MomentViewSet(viewsets.ModelViewSet):
-    queryset = Moment.objects.all()
     serializer_class = MomentSerializer
 
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated,
+        IsOwner
     ]
+
+    def get_queryset(self):
+        self.request.user.moments.all()
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -34,5 +57,5 @@ class PromptViewSet(viewsets.ModelViewSet):
     serializer_class = PromptSerializer
 
     permission_classes = [
-        permissions.AllowAny
+        IsAdminOrReadOnly
     ]
--- back_end/moments/moments/urls.py
+++ back_end/moments/moments/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/auth/', include("accounts.urls")),
+    path('', include("rest_framework.urls")),
     path('', include("journal.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
