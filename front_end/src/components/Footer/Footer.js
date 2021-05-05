7cf48f6b7c393e85845368947cae223b2564929c
Kashan Sheikh <65731465+k-sheikh@users.noreply.github.com>
2021-05-05 11:24:17 +0100

    Add icons to footer

--- front_end/src/components/Footer/Footer.js
+++ front_end/src/components/Footer/Footer.js
@@ -1,9 +1,26 @@
 import './Footer.css'
+import { FaFacebookSquare,  FaInstagram, FaGithub, FaTwitter, FaEnvelope } from 'react-icons/fa'
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="footer-text">&#169; Copyright Moments 2021</p>
+            <h1>
+                <FaFacebookSquare style={{cursor:"pointer"}} />
+            </h1>
+            <h1>
+            <FaInstagram style={{cursor:"pointer"}} />
+            </h1>
+            <h1>
+                <FaTwitter style={{cursor:"pointer"}} />
+            </h1>
+            <h1>
+                <FaEnvelope style={{cursor:"pointer"}} />
+            </h1>
+            <h1>
+                <FaGithub style={{cursor:"pointer"}} />    
+            </h1>
+            <p>&#169; Copyright Moments 2021</p>
+            {/* <p className="footer-text">&#169; Copyright Moments 2021</p> */}
         </footer>
     ) 
 }
