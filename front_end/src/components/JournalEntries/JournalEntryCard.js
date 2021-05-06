592a8c56b7356fbe719610544a41ae207612c403
Ozan Caglar <52323924+ozancaglar@users.noreply.github.com>
2021-05-06 20:24:02 +0100

    Journalling updates

--- front_end/src/components/JournalEntries/JournalEntryCard.js
+++ front_end/src/components/JournalEntries/JournalEntryCard.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Icon from '../Icon/Icon'
+import './Journalling.css'
 const JournalEntryCard = ({id, onDelete, body, title, image, date, prompt}) => {
     return (
-        <div className="journal-entry-card">
-            <p>{prompt}</p>
-            <p>{title}</p>
+        <div className="lastSelector">
+        <div className="card">
+            {prompt ? <h3>{prompt}</h3> : <h3>{title}</h3>}
+            <div className="journal-text">
             <p>{body}</p>
             <p>{date}</p>
+            </div>
             <img src={image} alt="" className="entry-image"/>
-            <Icon 
+            {!body ? null : <Icon 
             className="deckIcon"
             event={()=>onDelete(id)}
-            />
+            />}
+            
+        </div>
         </div>
     )
 }
